9b71114b244fbee4f77ec37027bab4dc0cfdd1fb
Cao Qihao Company <caoqihao88@gmail.com>
2025-03-17 18:06:25 +0800

    98 3:22

--- movie/views.py
+++ movie/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
 from .models import Movie
+from .serializers import MovieListSerializer
 
 
 # Create your views here.
+@api_view(['GET','POST'])
 def movie_list(request):
-    # 获取数据
-    # movies = Movie.objects.all().values_list()
-    # data = list(movies)
-    data = {
-        'movie_name':'碟中谍',
-        'rate':7.8
-    }
-    return JsonResponse(data , safe=False)
+    if request.method == 'GET':
+        # 获取数据
+        movies = Movie.objects.all()
+        serializer = MovieListSerializer(movies , many=True)   
+        return Response(serializer.data,status=status.HTTP_200_OK)
